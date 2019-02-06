966a07c57dcd16255883d1ac4e53b8be57b3cc33
Marcelo Caldas <mcq1@cdc.gov>
2019-02-06 12:11:35 -0500

    fixing instrument name

--- src/main/java/gov/cdc/fhir/bser/redcap/model/RedCapFeedbackInstrument.java
+++ src/main/java/gov/cdc/fhir/bser/redcap/model/RedCapFeedbackInstrument.java
@@ -14,12 +14,12 @@ public class RedCapFeedbackInstrument {
     private String record;
     private String redcap_event_name;
     private String instrument;
-    private String create_feedback_complete;
+    private String dpp_feedback_complete;
 
     private final static String REDCAP_EVENT_CREATE_FEEDBACK = "create_feedback_arm_1";
 
     // This variable is set to 2 for Completed Feedback form and the event name is create_feedback_arm_1
     public boolean isFeedBackTrigger() {
-        return REDCAP_EVENT_CREATE_FEEDBACK.equalsIgnoreCase(redcap_event_name) && "2".equals(create_feedback_complete);
+        return REDCAP_EVENT_CREATE_FEEDBACK.equalsIgnoreCase(redcap_event_name) && "2".equals(dpp_feedback_complete);
     }
 }
